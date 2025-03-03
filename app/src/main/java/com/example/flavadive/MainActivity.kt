d895f779102b70a2f380319bb4657b9831749299
dahee <ekgml9532@gmail.com>
2025-03-03 20:48:07 +0900

    add login screen

--- app/src/main/java/com/example/flavadive/MainActivity.kt
+++ app/src/main/java/com/example/flavadive/MainActivity.kt
@@ -3,44 +3,162 @@ package com.example.flavadive
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.flavadive.ui.theme.FlavaDiveTheme
+import com.example.flavadive.ui.theme.notosanskr
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FlavaDiveTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            LoginScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun LoginScreen() {
+    Column(
+        modifier = Modifier.padding(35.dp)
+    ) {
+        Spacer(modifier = Modifier.height(190.dp))
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FlavaDiveTheme {
-        Greeting("Android")
+        Text(
+            modifier = Modifier
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            text = "FlavaDive",
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Light,
+            fontStyle = FontStyle.Italic,
+        )
+
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = { Text(text = "아이디")},
+            modifier = Modifier
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(10.dp),
+        )
+
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = { Text(text = "비밀번호")},
+            modifier = Modifier
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(10.dp),
+        )
+
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(10.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Blue
+            )
+        ) {
+            Text("로그인")
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            TextButton(
+                onClick = { /*TODO*/ },
+            ) {
+                Text(
+                    text = "아이디 찾기",
+                    color = Color.Black
+                )
+            }
+            TextButton(
+                onClick = { /*TODO*/ },
+            ) {
+                Text(
+                    text = "비밀번호 찾기",
+                    color = Color.Black
+                )
+            }
+            TextButton(
+                onClick = { /*TODO*/ },
+            ) {
+                Text(
+                    text = "회원가입",
+                    color = Color.Black
+                )
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Image(
+                painter = painterResource(R.drawable.kakao_login_medium_narrow),
+                contentDescription = null,
+//                modifier = Modifier.size(80.dp),
+                contentScale = ContentScale.Crop,
+            )
+            Image(
+                painter = painterResource(R.drawable.android_light_sq_3x),
+                contentDescription = null,
+//                modifier = Modifier.size(80.dp),
+                contentScale = ContentScale.Crop,
+            )
+        }
     }
 }
+
