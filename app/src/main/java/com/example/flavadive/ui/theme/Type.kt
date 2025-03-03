ca8dffb5657d8fade4e7ed77d33d3ce04b482adc
dahee <ekgml9532@gmail.com>
2025-03-03 20:58:12 +0900

    add font family

--- app/src/main/java/com/example/flavadive/ui/theme/Type.kt
+++ app/src/main/java/com/example/flavadive/ui/theme/Type.kt
@@ -2,11 +2,25 @@ package com.example.flavadive.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.flavadive.R
 
 // Set of Material typography styles to start with
+val notosanskr = FontFamily(
+    Font(R.font.notosans_kr_thin, FontWeight.Thin, FontStyle.Normal),
+    Font(R.font.notosans_kr_extra_light, FontWeight.ExtraLight, FontStyle.Normal),
+    Font(R.font.notosans_kr_light, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.notosans_kr_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.notosans_kr_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.notosans_kr_semi_bold, FontWeight.SemiBold, FontStyle.Normal),
+    Font(R.font.notosans_kr_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.notosans_kr_extra_bold, FontWeight.ExtraBold, FontStyle.Normal),
+    Font(R.font.notosans_kr_black, FontWeight.Black, FontStyle.Normal),
+)
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
